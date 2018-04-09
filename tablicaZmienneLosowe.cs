83c52eca11b45ece26792e65071c3d9ddc470a39
Roman Zbroiński <romanzbroinski@gmail.com>
2018-04-09 11:46:39 +0200

    Zmienne losowe w tablicy
    Pierwszy zapis. Zmienne losowe w tablicy.

---
+++ tablicaZmienneLosowe.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Stwórz tablicę 3 x 4 z losowo generowanymi liczbami.
+             */
+            int[,] tablica3x4 = new int[3, 4];
+            Random rand = new Random();
+            for (int i = 0; i < tablica3x4.GetLength(0); i++)
+                {
+                for (int j = 0; j < tablica3x4.GetLength(0); j++)
+                {
+                    tablica3x4[i,j] = rand.Next(100);
+                    Console.Write("{0,4}", tablica3x4[i, j]);
+                }
+                Console.WriteLine();
+                }
+            Console.ReadKey();
+        }
+    }
+}
