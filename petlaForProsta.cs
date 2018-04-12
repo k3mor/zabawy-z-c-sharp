6f36ed834705ba7ea227955082380c57e00ccbc8
Roman Zbroiński <romanzbroinski@gmail.com>
2018-04-12 19:02:40 +0200

    Prosta pętla FOR
    Ta sama pętla z drobnymi zmianami

---
+++ petlaForProsta.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Wypisz liczby od 1 do 10 z użyciem pętli for
+            int a = 1;
+            for (int i = a; i < 11; i++)
+            {
+                
+                Console.WriteLine(i);
+                
+            }
+            //Wypisz w jednej kolumny liczby do 11 a w drugiej liczby o 1 wiekszą
+            Console.WriteLine("{0,8}{1,15}", "Liczba", "Dodaj 1");
+            for (int i = 0; i < 11; i++)
+            {
+                Console.WriteLine("{0,8}{1,15}", i, i + 1);
+
+            }
+            //Zwiększ liczbę o samą siebie
+            Console.WriteLine("{0,8}{1,15}", "Liczba", "Zwiększ");
+            for (int i = 0; i < 11; i++)
+            {
+                Console.WriteLine("{0,8}{1,15}", i, i + i);
+
+            }
+            //Pomnóż liczbę przez samą siebie
+            Console.WriteLine("{0,8}{1,15}", "Liczba", "Pomnóż");
+            for (int i = 0; i < 11; i++)
+            {
+                Console.WriteLine("{0,8}{1,15}", i, i * i);
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
