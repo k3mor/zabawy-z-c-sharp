ad3ed5a7d89a92e68029e8f7e7fcb4349429d25e
Roman Zbroiński <romanzbroinski@gmail.com>
2019-08-01 14:29:03 +0200

    foreach start
    Loop foreach start

---
+++ foreach1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace forEachStart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int[] liczby = { 1,2,3 };
+            foreach (int liczba in liczby)
+            {
+                System.Console.Write(liczba);
+            }
+            Console.ReadLine();
+        }
+        
+    }
+}
